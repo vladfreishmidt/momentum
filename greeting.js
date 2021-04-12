7a99cc9823151a9565176fae6182a8d8fc430ff6
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2021-04-12 10:41:03 +0300

    Added saveName() to localStorage

--- greeting.js
+++ greeting.js
@@ -7,6 +7,22 @@ const form = document.querySelector('.js-form'),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const currentValue = input.value;
+  saveName(currentValue);
+  paintGreeting(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -16,7 +32,7 @@ function paintGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    // Ask user to input name
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
